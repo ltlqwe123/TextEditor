fd006228747ed6fb317e76fde960b1c6b3316f85
ltlqwe123 <ltlqwe123@gmail.com>
2024-04-04 21:53:12 +0700

    TextEditor Project

---
+++ src/controller/Controller.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import view.textView;
+
+public class Controller implements ActionListener {
+    private textView tv;
+
+    public Controller(textView tv) {
+        this.tv = tv;
+    }
+
+    @Override
+    public synchronized void actionPerformed(ActionEvent e) {
+        Thread thread = new Thread(()->{
+            String src = e.getActionCommand();
+            if (src.equals("Luu")) {
+                tv.save();
+                return;
+            }
+            if (src.equals("Xoa")) {
+                tv.clean();
+                return;
+            }
+            if (src.equals("Doc file")) {
+                tv.read();
+                return;
+            }
+            if (src.equals("Dem slg tu")) {
+                tv.demsolgtu();
+            }
+        });
+        thread.start();
+    }
+}
---
+++ src/model/textModel.java
@@ -0,0 +1,17 @@
+package model;
+
+import javax.swing.plaf.FileChooserUI;
+import java.io.*;
+import java.util.ArrayList;
+
+public class textModel {
+    private String text = "";
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
